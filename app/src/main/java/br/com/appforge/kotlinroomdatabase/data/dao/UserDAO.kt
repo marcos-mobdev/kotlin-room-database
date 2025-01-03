404a26efbb8a0e0fb15475689322413f66d6666a
Marcos Pinheiro <marcospinheiro.em@gmail.com>
2025-01-02 21:08:59 -0300

    Added User DAO

---
+++ app/src/main/java/br/com/appforge/kotlinroomdatabase/data/dao/UserDAO.kt
@@ -0,0 +1,21 @@
+package br.com.appforge.kotlinroomdatabase.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+import br.com.appforge.kotlinroomdatabase.data.model.User
+
+@Dao
+interface UserDAO {
+    @Insert
+    fun save(user: User)
+
+    @Update
+    fun update(user:User)
+
+    @Delete
+    fun delete(user:User)
+
+
+}
