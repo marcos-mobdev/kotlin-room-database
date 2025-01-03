8cb2859e01f7dfc1a9051401f4d4cff5b0450cfc
Marcos Pinheiro <marcospinheiro.em@gmail.com>
2025-01-02 21:04:16 -0300

    Added Layout and Entity
    - Added layout with textfields and buttons for CRUD.
    - Added user entity

---
+++ app/src/main/java/br/com/appforge/kotlinroomdatabase/data/model/User.kt
@@ -0,0 +1,24 @@
+package br.com.appforge.kotlinroomdatabase.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+
+//snake case for table names
+@Entity(tableName = "users",
+    //ignoredColumns = ["name", "password"]
+
+    )
+class User(
+    @PrimaryKey(autoGenerate = true)
+    val userId:String,
+    val email:String,
+    @ColumnInfo("firstname_surname")
+    val name:String,
+    val password:String,
+    val age:Int,
+    val weight: Double,
+    //@Ignore val imc: Double
+    )
+{}
